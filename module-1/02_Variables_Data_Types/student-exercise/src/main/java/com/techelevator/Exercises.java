72478cd64394368c6e4d838716528b98d9b45e34
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-01-12 21:22:27 -0500

    Completed up to Problem 16, Blank Code Inserted As Placeholders

--- module-1/02_Variables_Data_Types/student-exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/02_Variables_Data_Types/student-exercise/src/main/java/com/techelevator/Exercises.java
@@ -13,7 +13,7 @@ public class Exercises {
 		int initialNumberOfBirds = 4;
 		int birdsThatFlewAway = 1;
 		int remainingNumberOfBirds = initialNumberOfBirds - birdsThatFlewAway;
-
+		System.out.println(remainingNumberOfBirds);
         /*
         2. There are 6 birds and 3 nests. How many more birds are there than
         nests?
@@ -23,220 +23,359 @@ public class Exercises {
 		int numberOfBirds = 6;
 		int numberOfNests = 3;
 		int numberOfExtraBirds = numberOfBirds - numberOfNests;
-
+		System.out.println(numberOfExtraBirds);
+		
         /*
         3. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
         many raccoons are left in the woods?
         */
-
+		int raccoonsPlayingInTheWoods = 3;
+		int raccoonsThatWentHomeForDinner = 2;
+		int racconsLeftInTheWoods = raccoonsPlayingInTheWoods - raccoonsThatWentHomeForDinner;
+		System.out.println(racconsLeftInTheWoods);
+		
         /*
         4. There are 5 flowers and 3 bees. How many less bees than flowers?
         */
-
+		int numberOfFlowers = 5;
+		int numberOfBees = 3;
+		int numberOfFlowersMinusBees = numberOfFlowers - numberOfBees;
+		System.out.println(numberOfFlowersMinusBees);
+		
         /*
         5. 1 lonely pigeon was eating breadcrumbs. Another pigeon came to eat
         breadcrumbs, too. How many pigeons are eating breadcrumbs now?
         */
-
+		int numberOfPigeons = 1;
+		numberOfPigeons++;
+		System.out.println(numberOfPigeons);
+		
         /*
         6. 3 owls were sitting on the fence. 2 more owls joined them. How many
         owls are on the fence now?
         */
-
+		int owlsSittingOnFence = 3;
+		int owlsThatJoinOnFence = 2;
+		owlsSittingOnFence += owlsThatJoinOnFence;
+		System.out.println(owlsSittingOnFence);
+	
         /*
         7. 2 beavers were working on their home. 1 went for a swim. How many
         beavers are still working on their home?
         */
+		int beaversWorkingOnHome = 2;
+		int beaversWentForASwim = 1;
+		int beaversStillWorkingOnHome = beaversWorkingOnHome - beaversWentForASwim;
 
         /*
         8. 2 toucans are sitting on a tree limb. 1 more toucan joins them. How
         many toucans in all?
         */
+		int toucansSittingOnTreeLimb = 2;
+		int toucansThatJoinOnTreeLimb = 1;
+		toucansSittingOnTreeLimb += toucansThatJoinOnTreeLimb;
+		System.out.println(toucansSittingOnTreeLimb);
 
         /*
         9. There are 4 squirrels in a tree with 2 nuts. How many more squirrels
         are there than nuts?
         */
+		int numberOfSquirrels = 4;
+		int numberOfNuts = 2;
+		int howManyMoreSquirrelsThanNuts = numberOfSquirrels - numberOfNuts;
+		System.out.println(howManyMoreSquirrelsThanNuts);
 
         /*
         10. Mrs. Hilt found a quarter, 1 dime, and 2 nickels. How much money did
         she find?
         */
-
-        /*
+		float numberOfQuarters = 1.0;
+		float numberOfDimes = 1.0;
+		float numberOfNickels = 2.0;
+		float totalAmountOfMoneyInDollars = numberOfQuarters * .25 + numberOfDimes * .10 + numberOfNickels * .5;
+		System.out.println(totalAmountOfMoneyInDollars);
+		
+		/*
         11. Mrs. Hilt's favorite first grade classes are baking muffins. Mrs. Brier's
         class bakes 18 muffins, Mrs. MacAdams's class bakes 20 muffins, and
         Mrs. Flannery's class bakes 17 muffins. How many muffins does first
         grade bake in all?
         */
+		int numberOfMuffinsMrsBrierClass = 18;
+		int numberOfMuffinsMrsMacadamsClass = 20;
+		int numberOfMuffinsMrsFlanneryClass = 17;
+		int totalNumberOfMuffinsFirstGrade = numberOfMuffinsMrsBrierClass + numberOfMuffinsMrsMacadamsClass + numberOfMuffinsMrsFlanneryClass;
+		System.out.println(totalNumberOfMuffinsFirstGrade);
 
         /*
         12. Mrs. Hilt bought a yoyo for 24 cents and a whistle for 14 cents. How
         much did she spend in all for the two toys?
         */
+		int priceOfYoyoInCents = 24;
+		int priceOfWhistleInCents = 14;
+		int totalSpentOnToysInCents = priceOfYoyoInCents + priceOfWhistleInCents;
+		System.out.println(totalSpentOnToysInCents);
 
         /*
         13. Mrs. Hilt made 5 Rice Krispie Treats. She used 8 large marshmallows
         and 10 mini marshmallows.How many marshmallows did she use
         altogether?
         */
+		int numberOfLargeMarshmallow = 8;
+		int numberOfMiniMarshmallow = 10;
+		int totalNumberOfMarshmallow = numberOfLargeMarshmallow + numberOfMiniMarshmallow;
+		System.out.println(totalNumberOfMarshmallow);
 
         /*
         14. At Mrs. Hilt's house, there was 29 inches of snow, and Brecknock
         Elementary School received 17 inches of snow. How much more snow
         did Mrs. Hilt's house have?
         */
-
+		int inchesOfSnowfallMrsHiltHouse = 29;
+		int inchesOfSnowfallBrecknockSchool = 17;
+		int differenceInSnowfallInInches = inchesOfSnowfallMrsHiltHouse - inchesOfSnowfallBrecknockSchool;
+		System.out.println(differenceInSnowfallInInches);
+		
         /*
         15. Mrs. Hilt has $10. She spends $3 on a toy truck and $2 on a pencil
         case. How much money does she have left?
         */
+		int startingDollarAmountMrsHilt = 10;
+		int priceToyTruckInDollars = 3;
+		int pricePencilInDollars = 2;
+		int currentDollarAmountMrsHilt = startingDollarAmountMrsHilt - priceToyTruckInDollars - pricePencilInDollars;
+		System.out.println(currentDollarAmountMrsHilt);
 
         /*
         16. Josh had 16 marbles in his collection. He lost 7 marbles. How many
         marbles does he have now?
         */
 
+		int numberOf
+		int numberOf
+		int numberOf
+		
         /*
         17. Megan has 19 seashells. How many more seashells does she need to
         find to have 25 seashells in her collection?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         18. Brad has 17 balloons. 8 balloons are red and the rest are green. How
         many green balloons does Brad have?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         19. There are 38 books on the shelf. Marta put 10 more books on the shelf.
         How many books are on the shelf now?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         20. A bee has 6 legs. How many legs do 8 bees have?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         21. Mrs. Hilt bought an ice cream cone for 99 cents. How much would 2 ice
         cream cones cost?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         22. Mrs. Hilt wants to make a border around her garden. She needs 125
         rocks to complete the border. She has 64 rocks. How many more rocks
         does she need to complete the border?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         23. Mrs. Hilt had 38 marbles. She lost 15 of them. How many marbles does
         she have left?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         24. Mrs. Hilt and her sister drove to a concert 78 miles away. They drove 32
         miles and then stopped for gas. How many miles did they have left to drive?
         */
-
+		int numberOf
+		int numberOf
+		int numberOf
+		
         /*
         25. Mrs. Hilt spent 1 hour and 30 minutes shoveling snow on Saturday
         morning and 45 minutes shoveling snow on Saturday afternoon. How
         much total time did she spend shoveling snow?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         26. Mrs. Hilt bought 6 hot dogs. Each hot dog cost 50 cents. How much
         money did she pay for all of the hot dogs?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         27. Mrs. Hilt has 50 cents. A pencil costs 7 cents. How many pencils can
         she buy with the money she has?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         28. Mrs. Hilt saw 33 butterflies. Some of the butterflies were red and others
         were orange. If 20 of the butterflies were orange, how many of them
         were red?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         29. Kate gave the clerk $1.00. Her candy cost 54 cents. How much change
         should Kate get back?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         30. Mark has 13 trees in his backyard. If he plants 12 more, how many trees
         will he have?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         31. Joy will see her grandma in two days. How many hours until she sees
         her?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         32. Kim has 4 cousins. She wants to give each one 5 pieces of gum. How
         much gum will she need?
         */
-
+		int numberOf
+		int numberOf
+		int numberOf
+		
         /*
         33. Dan has $3.00. He bought a candy bar for $1.00. How much money is
         left?
         */
-
+		int numberOf
+		int numberOf
+		int numberOf
+		
         /*
         34. 5 boats are in the lake. Each boat has 3 people. How many people are
         on boats in the lake?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         35. Ellen had 380 legos, but she lost 57 of them. How many legos does she
         have now?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         36. Arthur baked 35 muffins. How many more muffins does Arthur have to
         bake to have 83 muffins?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         37. Willy has 1400 crayons. Lucy has 290 crayons. How many more
         crayons does Willy have then Lucy?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         38. There are 10 stickers on a page. If you have 22 pages of stickers, how
         many stickers do you have?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         39. There are 96 cupcakes for 8 children to share. How much will each
         person get if they share the cupcakes equally?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         40. She made 47 gingerbread cookies which she will distribute equally in
         tiny glass jars. If each jar is to contain six cookies each, how many
         cookies will not be placed in a jar?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         41. She also prepared 59 croissants which she plans to give to her 8
         neighbors. If each neighbor received and equal number of croissants,
         how many will be left with Marian?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         42. Marian also baked oatmeal cookies for her classmates. If she can
         place 12 cookies on a tray at a time, how many trays will she need to
         prepare 276 oatmeal cookies at a time?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         43. Marian’s friends were coming over that afternoon so she made 480
         bite-sized pretzels. If one serving is equal to 12 pretzels, how many
         servings of bite-sized pretzels was Marian able to prepare?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         44. Lastly, she baked 53 lemon cupcakes for the children living in the city
@@ -244,23 +383,35 @@ public class Exercises {
         boxes with 3 lemon cupcakes each were given away?
         */
 
+		int numberOf
+		int numberOf
+		int numberOf
         /*
         45. Susie's mom prepared 74 carrot sticks for breakfast. If the carrots
         were served equally to 12 people, how many carrot sticks were left
         uneaten?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         46. Susie and her sister gathered all 98 of their teddy bears and placed
         them on the shelves in their bedroom. If every shelf can carry a
         maximum of 7 teddy bears, how many shelves will be filled?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         47. Susie’s mother collected all family pictures and wanted to place all of
         them in an album. If an album can contain 20 pictures, how many
         albums will she need if there are 480 pictures?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         48. Joe, Susie’s brother, collected all 94 trading cards scattered in his
@@ -268,17 +419,26 @@ public class Exercises {
         cards, how many boxes were filled and how many cards are there in
         the unfilled box?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         49. Susie’s father repaired the bookshelves in the reading room. If he has
         210 books to be distributed equally on the 10 shelves he repaired,
         how many books will each shelf contain?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
         50. Cristina baked 17 croissants. If she planned to serve this equally to
         her seven guests, how many will each have?
         */
+		int numberOf
+		int numberOf
+		int numberOf
 
         /*
             CHALLENGE PROBLEMS
