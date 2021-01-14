0138addcf32848c339570b94a11bc21d7203b40a
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-01-14 14:21:16 -0500

    11 problems solved. 4 remaining problems involve returning arrays, need to study to complete.

--- module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
+++ module-1/04_Loops_Arrays/student-exercise/src/main/java/com/techelevator/Exercises.java
@@ -10,7 +10,7 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
-		return false;
+		return( nums[0] == 6 || nums[nums.length - 1] == 6) ? true : false;
 	}
 
 	/*
@@ -21,15 +21,15 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
-		return false;
-	}
+		return(nums.length <=1 || nums[0] != nums[ nums.length - 1 ]) ? false : true;
+	} //if array length is 1 or less, fail || if the first and last element are not equal, fail
 
 	/*
 	 3. Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+		return new int[] { 3, 1, 4 };
 	}
 
 	/*
@@ -40,8 +40,8 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
-		return false;
-	}
+		return( a[0] == b[0] || a[a.length - 1] == b[b.length -1]) ? true : false;
+	} //if first elements in a[] and b[] match, pass || if last elements in a[] and b[] match, pass
 
 	/*
 	 5. Given an array of ints length 3, return the sum of all the elements.
@@ -50,7 +50,11 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+		int sumOfNumbers = 0;
+		for(int i=0; i<nums.length; i++) {
+			sumOfNumbers += nums[i];
+		}
+		return sumOfNumbers;
 	}
 
 	/*
@@ -94,7 +98,11 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+		if( nums.length == 0) {
+			return 0; // automatically return zero
+		} else if( nums.length == 1) {
+			return nums[0]; // automatically return only value
+		} return nums[0] + nums[1]; // final defaults to returning sum of first two values
 	}
 
 	/*
@@ -116,7 +124,12 @@ public class Exercises {
 	 countEvens([1, 3, 5]) → 0
 	 */
 	public int countEvens(int[] nums) {
-		return 0;
+		int counterOfEvens = 0;
+		for( int i=0; i<nums.length; i++) {
+			if( nums[i] % 2 == 0) {
+				counterOfEvens++;
+			}
+		} return counterOfEvens;
 	}
 
 	/*
@@ -130,7 +143,12 @@ public class Exercises {
 	 sum13([1, 2, 2, 1, 13, 3, 4]) → 10
 	 */
 	public int sum13(int[] nums) {
-		return 0;
+		int sumOfNumbers = 0;
+		for( int i=0; i<nums.length; i++) {
+			if( nums[i] == 13) {
+				i++;
+			} else sumOfNumbers += nums[i];
+		} return sumOfNumbers;
 	}
 
 	/*
@@ -140,7 +158,11 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
-		return false;
+		for( int i=0; i<nums.length; i++) {
+			if( ( i+1 < nums.length) && (nums[i] == 2) && (nums[i+1] == 2) ) { 
+				return true;
+			}
+		} return false;
 	}
 	
 	/*
@@ -150,7 +172,11 @@ public class Exercises {
 	 lucky13([1, 2, 4]) → false
 	 */
 	public boolean lucky13(int[] nums) {
-		return false;
+		for( int i=0; i<nums.length; i++) {
+			if( nums[i] == 1 || nums[i] == 3 ) {
+				return false;
+			} 
+		} return true;
 	}
 
 	/*
@@ -160,7 +186,12 @@ public class Exercises {
 	 sum28([1, 2, 3, 4]) → false
 	 */
 	public boolean sum28(int[] nums) {
-		return false;
+		int sumOfTwos = 0;
+		for( int i=0; i<nums.length; i++ ) {
+			if( nums[i] == 2) {
+				sumOfTwos += 2;
+			}
+		} return sumOfTwos == 8 ? true : false;
 	}
 
 }
