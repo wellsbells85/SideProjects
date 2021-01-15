4fc41e7befc5cecdf9e667d53e1ba96c58dd21c4
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-01-15 14:05:21 -0500

    Completed TempConvert.java. Upload working program.

--- module-1/05_Command_Line_Programs/student-lecture/src/main/java/com/techelevator/DiscountCalculator.java
+++ module-1/05_Command_Line_Programs/student-lecture/src/main/java/com/techelevator/DiscountCalculator.java
@@ -16,12 +16,21 @@ class DiscountCalculator {
         // Prompt the user for a discount amount
         // The answer needs to be saved as a double
         System.out.print("Enter the discount amount (w/out percentage): ");
-
+        String discountAsString = scanner.nextLine();
+        double discountAsDouble = Double.parseDouble(discountAsString);
 
 
         // Prompt the user for a series of prices
         System.out.print("Please provide a series of prices (space separated): ");
-
+        String seriesOfPrices = scanner.nextLine();
+        String[] pricesSeparated = seriesOfPrices.split(" ");
+        
+        for( int i=0; i < pricesSeparated.length; i++ ) {
+        	String pricesAsString = pricesSeparated[i];
+        	double price = Double.parseDouble(pricesAsString);
+        	
+        	System.out.println("Price: " + price + "\t" + "Discount Prices: " + (price * discountAsDouble));
+        }
 
 
 
