c4b76b0d6c1b4cc291f64a470557712992e0b271
Tom Medvitz <tom@techelevator.com>
2021-01-19 15:00:02 -0500

    Adding OPTIONAL exercises for Stacks and Queues

---
+++ module-1/07_Collections_Part_1/student-exercise/src/main/java/com/techelevator/StackAndQueueExercises.java
@@ -0,0 +1,33 @@
+package com.techelevator;
+
+import java.util.List;
+
+public class StackAndQueueExercises {
+
+	/*
+	 Given a List of Strings, return a new list in reverse order of the original. One obvious solution is to
+	 simply loop through the original list in reverse order, but see if you can come up with an alternative
+	 solution. (Hint: Think LIFO (i.e. stack))
+	 reverseList( ["purple", "green", "blue", "yellow", "green" ])  -> ["green", "yellow", "blue", "green", "purple" ]
+	 reverseList( ["jingle", "bells", "jingle", "bells", "jingle", "all", "the", "way"} )
+		-> ["way", "the", "all", "jingle", "bells", "jingle", "bells", "jingle"]
+	 */
+	public List<String> reverseList(List<String> stringList) {
+		return null;
+	}
+	
+	/*
+	 Given a list of Integers representing seat numbers, group them into ranges 1-10, 11-20, and 21-30.
+	 (Any seat number less than 1, or greater than 30 is invalid, and can be ignored.) Preserve the order
+	 in which the seat number entered their associated group. Return a list of the grouped Integers 1-10,
+	 11-20, and 21-30. (Hint: Think multiple queues)
+	 boardingGate( [1, 13, 43, 22, 8, 11, 30, 2, 4, 14, 21] ) -> [1, 8, 2, 4, 13, 11, 14, 22, 30, 21]
+	 boardingGate( [29, 19, 9, 21, 11, 1, 0, 25, 15, 5, 31] ) -> [9, 1, 5, 19, 11, 15, 29, 21, 25]
+	 boardingGate( [0, -1, 44, 31, 17, 7, 27, 16, 26, 6] ) -> [7, 6, 17, 16, 27, 26]
+	 */
+	public List<Integer> boardingGate(List<Integer> seatNumberList) {
+		return null;
+	}
+
+
+}
