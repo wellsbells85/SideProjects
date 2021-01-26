01c05eae6c5adad49c207ccb8027bcf76d27b917
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-01-25 20:39:02 -0500

    SavingsAccount.java completed. 7/7 Runs, 0 Errors, 0 Failures.

---
+++ module-1/11_Inheritance/student-exercise/src/main/java/com/techelevator/SavingsAccount.java
@@ -0,0 +1,24 @@
+package com.techelevator;
+
+public class SavingsAccount extends BankAccount {
+
+	public SavingsAccount(String accountNumber, String accountHolderName) {
+		super(accountNumber, accountHolderName);
+		super.getBalance();
+	}
+	
+	public SavingsAccount(String accountNumber, String accountHolderName, int balance) {
+		super(accountNumber, accountHolderName, balance);
+	}
+	
+	@Override
+	public int withdraw(int amountToWithdraw) {
+		if(super.getBalance() - amountToWithdraw < 0) {
+			return super.getBalance();
+		} else if( super.getBalance() - amountToWithdraw < 150) {
+			return super.withdraw(amountToWithdraw + 2);
+		} else {
+			return super.withdraw(amountToWithdraw);
+		}
+	}
+}
