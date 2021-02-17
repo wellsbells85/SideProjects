2c3bd14714d8b9310777e94b36942d8d2ccfbcc5
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-02-17 10:11:24 -0500

    Uploading in class lecture note edits.

--- module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCDepartmentDAO.java
+++ module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCDepartmentDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Department;
 import com.techelevator.projects.model.DepartmentDAO;
@@ -20,27 +21,76 @@ public class JDBCDepartmentDAO implements DepartmentDAO {
 
 	@Override
 	public List<Department> getAllDepartments() {
-		return new ArrayList<>();
+		ArrayList<Department> departments = new ArrayList<>();
+		String sqlFindAllDepartments = "SELECT department_id, name" +
+									   "FROM department";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindAllDepartments);
+		if(results.next()) {
+			Department theDepartment = mapRowToDepartment(results);
+			departments.add(theDepartment);
+		} return departments;
 	}
 
 	@Override
 	public List<Department> searchDepartmentsByName(String nameSearch) {
-		return new ArrayList<>();
+		ArrayList<Department> departments = new ArrayList<>();
+		String sqlFindDepartmentsByName = "SELECT department_id, name" +
+										  "FROM department" +
+										  "WHERE name = ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindDepartmentsByName, nameSearch);
+		if(results.next()) {
+			Department theDepartment = mapRowToDepartment(results);
+			departments.add(theDepartment);
+		}
+		return departments;
 	}
 
 	@Override
 	public void saveDepartment(Department updatedDepartment) {
+		String sqlInsertDepartment = "INSERT INTO department(department_id, name) " +
+									 "VALUES (?,?)";
 		
+		jdbcTemplate.update(sqlInsertDepartment, updatedDepartment.getId(), updatedDepartment.getName());
 	}
 
 	@Override
 	public Department createDepartment(Department newDepartment) {
+		String sqlInsertDepartment = "INSERT INTO department(department_id, name) " +
+				 "VALUES (?,?)";
+
+		newDepartment.setId(getNextDepartmentId());
+		jdbcTemplate.update(sqlInsertDepartment, newDepartment.getId(), newDepartment.getName());
 		return null;
 	}
 
 	@Override
 	public Department getDepartmentById(Long id) {
-		return null;
+		Department theDepartment = null;
+		String sqlFindDepartmentById = "SELECT department_id, name" +
+									   "FROM department" +
+									   "WHERE department_id = ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindDepartmentById, id);
+		if(results.next()) {
+			theDepartment = mapRowToDepartment(results);
+		} return theDepartment;
+	}
+	
+	private long getNextDepartmentId() {
+		SqlRowSet nextIdResult = jdbcTemplate.queryForRowSet("SELECT nextval('seq_department_id')");
+		if(nextIdResult.next()) {
+			return nextIdResult.getLong(1);
+		} else {
+			throw new RuntimeException("Something went wrong while getting an id for the new city");
+		}
+	}
+	
+	private Department mapRowToDepartment(SqlRowSet results) {
+		Department theDepartment;
+		theDepartment = new Department();
+		theDepartment.setId(results.getLong("department_id"));
+		theDepartment.setName(results.getString("name"));
+		return theDepartment;
 	}
 
 }
--- module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
+++ module-2/07_Database_Connectivity_DAO/student-exercise/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
@@ -1,11 +1,13 @@
 package com.techelevator.projects.model.jdbc;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import com.techelevator.projects.model.Employee;
 import com.techelevator.projects.model.EmployeeDAO;
@@ -20,17 +22,44 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 	
 	@Override
 	public List<Employee> getAllEmployees() {
-		return new ArrayList<>();
+		ArrayList<Employee> employees = new ArrayList<>();
+		String sqlFindAllEmployees = "SELECT employee_id, department_id, first_name, last_name, birth_date, gender, hire_date " +
+									 "FROM employee";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindAllEmployees);
+		if(results.next()) {
+			Employee theEmployee = mapRowToEmployee(results);
+			employees.add(theEmployee);
+		} return employees;
 	}
 
 	@Override
 	public List<Employee> searchEmployeesByName(String firstNameSearch, String lastNameSearch) {
-		return new ArrayList<>();
+		ArrayList<Employee> employees = new ArrayList<>();
+		String sqlFindAllEmployees = "SELECT employee_id, department_id, first_name, last_name, birth_date, gender, hire_date " +
+									 "FROM employee " +
+									 "WHERE first_name = ? " +
+									 "AND last_name = ? ";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindAllEmployees, firstNameSearch, lastNameSearch);
+		if(results.next()) {
+			Employee theEmployee = mapRowToEmployee(results);
+			employees.add(theEmployee);
+		} return employees;
 	}
 
 	@Override
 	public List<Employee> getEmployeesByDepartmentId(long id) {
-		return new ArrayList<>();
+		ArrayList<Employee> employees = new ArrayList<>();
+		String sqlFindAllEmployees = "SELECT employee_id, department_id, first_name, last_name, birth_date, gender, hire_date " +
+									 "FROM employee " +
+									 "WHERE department_id = ? ";
+		
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlFindAllEmployees, id);
+		if(results.next()) {
+			Employee theEmployee = mapRowToEmployee(results);
+			employees.add(theEmployee);
+		} return employees;
 	}
 
 	@Override
@@ -46,6 +75,25 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 	@Override
 	public void changeEmployeeDepartment(Long employeeId, Long departmentId) {
 		
+	}
+	
+	private Employee mapRowToEmployee(SqlRowSet results) {
+		Employee theEmployee;
+		theEmployee = new Employee();
+		theEmployee.setId(results.getLong("employee_id"));
+		theEmployee.setDepartmentId(results.getLong("department_id"));
+		theEmployee.setFirstName(results.getString("first_name"));
+		theEmployee.setLastName(results.getString("last_name"));
+		theEmployee.setGender(results.getString("gender").charAt(0));
+		try {
+			theEmployee.setBirthDay(LocalDate.parse(results.getString("birth_date")));
+		} catch (Exception e){	
+		} //end try-catch
+		try {
+			theEmployee.setHireDate(LocalDate.parse(results.getString("hire_date")));
+		} catch (Exception e){	
+		} //end try-catch
+		return theEmployee;
 	}
 
 }
