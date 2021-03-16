5752fe89221b7006e4365b1ac348790231254148
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-03-15 22:57:31 -0400

    Completed both challenge problems for Monday Night

--- module-3/06_Intro_to_JavaScript/student-exercise-oops/challenge-exercises.js
+++ module-3/06_Intro_to_JavaScript/student-exercise-oops/challenge-exercises.js
@@ -11,6 +11,28 @@
 		iqTest("") → 0 // there are no numbers in the given set
         iqTest("2 2 4 6") → 0 // all numbers are even, therefore there is no position of an odd number
 */
+    function iqTest(x) {
+        if(x.length == 0) {
+            return 0;
+        }
+        let string = x.split(" ");
+        let evens = [];
+        let odds = [];
+        for(i = 0; i < string.length; i++) {
+            if(string[i]%2 == 0) {
+                evens.push(i+1);
+            } else if(string[i]%2 == 1) {
+                odds.push(i+1);
+            }
+        } 
+        if(evens.length == 1) {
+            return evens[0];
+        } else if(odds.length == 1) {
+            return odds[0];
+        } else {
+            return 0;
+        }
+    }
 
 /*
 2. **titleCase** Write a function that will convert a string into title case, given an optional 
@@ -28,3 +50,30 @@ argument is unused.
 		titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
         titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
 */
+        function titleCase(quote, minor) {
+            let words = quote.toLowerCase().split(" ");
+            if(minor == null) {
+                minor = " ";
+            }
+            let minors = minor.toLowerCase().split(" ");
+            
+            let sentence = "";
+            for(i = 0; i < words.length; i++) {
+                let title = true;
+                let word = words[i];
+                for(j = 0; j < minors.length; j++) {
+                    if(i > 0 && word == minors[j]) {
+                        title = false;
+                        break;
+                    }    
+                }
+                if(title == true) {
+                    let firstLetter = word.substring(0, 1).toUpperCase();
+                    word = firstLetter + word.substring(1, word.length) + " ";
+                    sentence += word;
+                } else {
+                    sentence += word + " ";
+                }       
+            }
+            return sentence.trim();
+        }    
