a10b12ab500fb1bbde4e6e82d51479f4d27716dc
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-03-31 11:13:24 -0400

    Small tweak to previously submitted homework to make it simpler, and to ensure that it would work correctly with the modification.

--- module-3/15_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
+++ module-3/15_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
@@ -3,7 +3,7 @@
       <h2 class="book-title">{{ book.title }}</h2>
       <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
       <h3 class="book-author">{{ book.author }}</h3>
-      <button v-bind:class="book.read?'mark-read':'mark-unread'" v-on:click="readOrNot(book)">{{ book.read?'Mark Unread':'Mark Read' }}</button>
+      <button v-bind:class="book.read?'mark-read':'mark-unread'" v-on:click="readOrNot">{{ book.read?'Mark Unread':'Mark Read' }}</button>
   </div>
 </template>
 
@@ -12,8 +12,8 @@ export default {
   name: 'book-card',
   props: ["book"],
   methods: {
-    readOrNot(book) {
-      this.$store.commit("FLIP_READ", book);
+    readOrNot() {
+      this.$store.commit("FLIP_READ", this.book);
     }
   }
 }
