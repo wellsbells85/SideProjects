cb8cb072ca24ef3ecb6a073f9beb0361d9ee5b0d
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-04-01 22:31:46 -0400

    Vue Routing Homework fully completed 10 of 10 tests passing.

--- module-3/17_Vue_Routing/student-exercise/src/components/ReadingList.vue
+++ module-3/17_Vue_Routing/student-exercise/src/components/ReadingList.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="book-container">
-      <book-card v-bind:book="book" v-for="book in $store.state.books" v-bind:key="book.isbn" /> 
+    <router-link :to="{ name: 'book-details', params: {isbn: b.isbn}}" v-for="b in $store.state.books" v-bind:key="b.isbn" >
+      <book-card v-bind:book="b"/> 
+    </router-link>  
   </div>
 </template>
 
--- module-3/17_Vue_Routing/student-exercise/src/views/BookDetails.vue
+++ module-3/17_Vue_Routing/student-exercise/src/views/BookDetails.vue
@@ -1,26 +1,22 @@
 <template>
-  <div class="details">
-    <h1>{{ book.title }}</h1>
-    <h1>{{ book.author }}</h1>
-    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
-    <h3>{{book.read ? "I have read this book!": "I have not read this book yet."}}</h3>
+  <div>
+    <book-details v-bind:book="theBook" />
   </div>
 </template>
 
 <script>
+import BookDetails from '@/components/BookDetails'
+
 export default {
-  name: 'book-details',
-  computed: {
-    book() {
-      const book = this.$store.state.books.find(
-        b => b.isbn == this.$store.state.activeBook
-      );
-      return book;
-    }
+  components: {
+    BookDetails
   },
-  created() {
-    const activeBookISBN = this.$route.params.isbn;
-    this.$store.commit("SET_ACTIVE_PRODUCT", activeBookISBN);
+  computed: {
+    theBook() {
+      return this.$store.state.books.find(
+        book => book.isbn == this.$route.params.isbn
+      );
+    }
   }
 };  
 </script>
